2759c8273c175a6d5cd2d12490eb7731317a85d2
翁誌璟 <s0968813171@gmail.com>
2025-05-06 16:41:58 +0800

    Add files via upload

---
+++ 9-2/9-2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace BoxCalculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "寬度:";
+
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "高度:";
+
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "長度:";
+
+            this.txtWidth.Location = new System.Drawing.Point(80, 27);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 23);
+            this.txtWidth.TabIndex = 3;
+
+            this.txtHeight.Location = new System.Drawing.Point(80, 67);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 23);
+            this.txtHeight.TabIndex = 4;
+
+            this.txtLength.Location = new System.Drawing.Point(80, 107);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 23);
+            this.txtLength.TabIndex = 5;
+
+            this.btnCalculate.Location = new System.Drawing.Point(80, 150);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 30);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "計算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "體積:";
+
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "面積:";
+
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(80, 200);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(0, 15);
+            this.lblVolume.TabIndex = 9;
+
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(80, 230);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(0, 15);
+            this.lblArea.TabIndex = 10;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 280);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "盒子計算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Label lblArea;
+    }
+}
---
+++ 9-2/9-2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BoxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double width = double.Parse(txtWidth.Text);
+                double height = double.Parse(txtHeight.Text);
+                double length = double.Parse(txtLength.Text);
+
+                Box box = new Box(width, height, length);
+
+                lblVolume.Text = box.Volume().ToString("F2");
+                lblArea.Text = box.Area().ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("請輸入有效的數字", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生錯誤: {ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Box
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Length { get; set; }
+
+        public Box(double width, double height, double length)
+        {
+            Width = width;
+            Height = height;
+            Length = length;
+        }
+
+        public double Volume()
+        {
+            return Width * Height * Length;
+        }
+
+        public double Area()
+        {
+            return 2 * (Width * Height + Width * Length + Height * Length);
+        }
+    }
+}
